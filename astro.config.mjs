01ab755619327a75c95517b630870e716710453d
Javier Garin <dev.javiergarin@gmail.com>
2025-05-14 17:34:34 -0400

    update build files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel'; // Importación actualizada
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    // Configuración para Serverless (ahora se especifica aquí)
+    isr: true // Opcional: habilita ISR si lo necesitas
+  }),
+  image: {
+    service: {
+      // Configuración actualizada para Squoosh
+      entrypoint: 'astro/assets/services/noop' // Solución temporal
+      // O usa:
+      // entrypoint: 'astro/assets/services/squoosh-client'
+    }
+  }
 });
