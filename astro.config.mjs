69f41ea7ec784b2c6ad0bbc9d1b84b7282cc3a51
Javier Garin <dev.javiergarin@gmail.com>
2025-05-14 21:19:37 -0400

    update build files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import vercel from '@astrojs/vercel';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-// Para obtener la ruta absoluta a `src`
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
@@ -21,6 +17,7 @@ export default defineConfig({
     }
   },
   vite: {
+    plugins: [tsconfigPaths()],
     resolve: {
       alias: {
         '@': new URL('./src', import.meta.url).pathname
